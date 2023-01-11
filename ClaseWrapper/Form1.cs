dd947972f5ee5a0ec6f0c99c37d7e632cbc4f333
svelazquezmoby <svelazquez@mobydigital.com>
2023-01-11 11:58:00 -0300

    firstcommit

---
+++ ClaseWrapper/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClaseWrapper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EngineWrapper ewrap = new EngineWrapper();
+            ewrap.UpdateRPM = "1200";
+            ewrap.PrintRPM();
+            ewrap.PrintString();
+        }
+        class Engine
+        {
+            private string RPM;
+            public string Rpmstring
+            {
+                get { return RPM; }
+                set { RPM = value; }
+            }
+            public void PrintRPM()
+            {
+                MessageBox.Show(RPM);
+            }
+        }
+
+        class EngineWrapper
+        {
+            private Engine engine;  
+            public EngineWrapper()
+            {
+                engine = new Engine();  
+            }
+            public string UpdateRPM
+            {
+                get { return engine.Rpmstring; }
+                set { engine.Rpmstring = value; }
+            }
+            public void PrintRPM()
+            {
+                engine.PrintRPM();
+            }
+            public void PrintString()
+            {
+                MessageBox.Show("local string");
+            }
+        }
+    }
+}
